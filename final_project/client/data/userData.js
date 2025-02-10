a866b1a7bdaea0f5538663f68e14472270f4f1da
Le Duc Phat <phat.lduc@gmail.com>
2025-02-10 10:04:30 +0700

    bài tập a03
    bài tập a03 cbi merge vào main

---
+++ final_project/client/data/userData.js
@@ -0,0 +1,10 @@
+export const userData = {
+  _id: 1,
+  profilePic: "https://cdn-icons-png.flaticon.com/512/149/149071.png",
+  name: "Le Duc Phat",
+  email: "kanedp09@gmail.com",
+  password: "1234567890",
+  address: "1 Vo Van Ngan",
+  city: "Ho Chi Minh City",
+  contact: "0903103597",
+};
